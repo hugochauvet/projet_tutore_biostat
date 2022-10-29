43ca3357a983e3787a9a838f37dfec37be82e279
hugochauvet <106102956+hugochauvet@users.noreply.github.com>
2022-10-29 13:23:42 +0200

    add .csv into the repository

---
+++ main.py
@@ -0,0 +1,13 @@
+##############################################################################
+##### SUPERVISED PROJECT BioStat M1 SSD
+##############################################################################
+
+#%% IMPORTING PACKAGES
+
+import numpy as np
+import pandas as pd
+import os
+
+#%% DATA IMPORT
+
+df = pd.read_csv("projet_tutore_biostat/expression_data_tsg3_3686_samples_20982_genes__head.csv")
