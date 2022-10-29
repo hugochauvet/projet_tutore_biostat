deb9b538591fee33a4e44fa28e999c8cc7aac97c
hugochauvet <106102956+hugochauvet@users.noreply.github.com>
2022-10-29 14:57:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,4 +10,7 @@ import os
 
 #%% DATA IMPORT
 
-df = pd.read_csv("expression_data_tsg3_3686_samples_20982_genes__head.csv")
+path = "C:/Users/33638/Documents/MasterSSD/M1/S7/Projet_biostat/"
+df = pd.read_csv(path + "expression_data_tsg3_3686_samples_20982_genes__head.csv", sep = ";", index_col = 0)
+print(df)
+print(df.shape)
